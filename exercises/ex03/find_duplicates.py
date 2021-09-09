da429e6ee4694dac600be2739507d28e11f72f78
Aaviele <aaviele@live.unc.edu>
2021-09-09 19:11:15 -0400

    Prorgess on Exercise 03

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -4,17 +4,14 @@ __author__ = "730322189"
 
 word: str =  input("Enter a word: ")
 i: int = 0
-boolean_var: bool = False
+k: int = 0
 
 while i < len(word):
-    j: int = 0
-    k: int = 0
+    j: int = i + 1
     while j < len(word):
         if word[i] == word[j]:
             k = k + 1
         j = j + 1
-        if k > 1:
-            boolean_var: bool = True
     i = i + 1
 
-print("Found Duplicate: " + str(boolean_var))
+print("Found Duplicate: " + str(k > 0))
