0e6266b583d8d059db33dc7073004703c71f7bde
Aaviele <aaviele@live.unc.edu>
2021-09-21 21:56:34 -0400

    Progress on CYOA

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -14,6 +14,9 @@ restart: bool
 
 
 def main() -> None:
+    global points
+    global player
+    global restart
     points = 0
     player = ""
     restart = True
@@ -21,28 +24,32 @@ def main() -> None:
     while restart == True:
         enter_the_game: str = input("Would you like to guess heads, guess tails, or end the game? Enter H for heads. Enter T for tails. Enter E for end game. ")
         if enter_the_game == "H":
-            num: int = int(input("Guess how many heads out of 10 coinflips: "))
-            points = heads_guess(num, points)
-            print(f"You have {points} adventure point(s).")
+            heads_guess()
+            menu()
         if enter_the_game == "T":
-            num: int = int(input("Guess how many tails out of 10 coinflips: "))
-            points = tails_guess(num, points)
+            points = tails_guess(points, player)
             print(f"You have {points} adventure point(s).")
-        if enter_the_game  ==  "E":
+            menu()
+        if enter_the_game == "E":
             print(f"You have earned {points} adventure point(s).")
-            print(f"Goodbye!")
+            print(f"Goodbye, {player}!")
             restart = False
 
 
-def heads_guess(num: int, points = int) -> None: 
+def heads_guess():
+    global points
+    global restart
+    global player
+    num: int = int(input(f"{player}, guess how many heads out of 10 coinflips: "))
     correct_heads: int = randint(0, 10)
     if num == correct_heads:
         points = points + 1
     print(f"The correct answer was {correct_heads}.")
-    return points
+    print(f"You have {points} adventure point(s).")
 
 
-def tails_guess(num: int, points: int) -> None:
+def tails_guess(points: int, player: str):
+    num: int = int(input(f"{player}, guess how many tails out of 10 coinflips: "))
     correct_tails: int = randint(0, 10)
     if num == correct_tails:
         points = points + 1
@@ -50,16 +57,21 @@ def tails_guess(num: int, points: int) -> None:
     return points
 
 
-def greet() -> None:
-    player: str = input("What is your name? ")
+def greet():
+    global player
+    player = input("What is your name? ")
     print(f"Welcome, {player}! {NAMED_CONSTANT} In this game, you  will try to guess the number of heads or tails out of 10 coinflips. For each correct guess, you will earn one adventure point.")
 
 
 def menu() -> None:
-    continue_game: str = input(f"{player}, Do you want to continue playing? Enter Y for yes. Enter N for no. ")
+    global restart
+    global player
+    continue_game: str = input(f"{player}, do you want to continue playing? Enter Y for yes. Enter N for no. ")
     if continue_game == "Y":
          restart = True
     if continue_game == "N":
+        print(f"You have earned {points} adventure point(s).")
+        print(f"Goodbye, {player}!")
         restart = False
 
 
