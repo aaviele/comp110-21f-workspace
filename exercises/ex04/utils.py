0d50ad87d734db9e80ffcaed275187e55d74cf82
Aaviele <aaviele@live.unc.edu>
2021-09-23 21:02:30 -0400

    Progress on Ex04

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -4,8 +4,10 @@ __author__ = "730322189"
 
 
 def all(integers: list[int], match: int) -> bool:
-    # Return True if all numbers match the indicated number and return False otherwise
+    """Return True if all numbers match the indicated number and return False otherwise."""
     i: int = 0
+    if len(integers) == 0:
+        return False
     while i < len(integers):
         item: int = integers[i]
         if item != match:
@@ -16,8 +18,12 @@ def all(integers: list[int], match: int) -> bool:
 
 
 def is_equal(list_1: list[int], list_2: list[int]) -> bool:
-    # Return True if every element at every index is equal in both lists
+    """Return True if every element at every index is equal in both lists."""
     i: int = 0
+    if len(list_1) == 0 and len(list_2) == 0:
+        return True
+    if len(list_1) == 0 or len(list_2) == 0:
+        return False
     while i <= len(list_1) or i <= len(list_2):
         item_1: int = list_1[i]
         item_2: int = list_2[i]
@@ -31,12 +37,12 @@ def is_equal(list_1: list[int], list_2: list[int]) -> bool:
 
 
 def max(input: list[int]) -> int:
-    # Return largest integer in the list
+    """Return largest integer in the list."""
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
     
     i: int = 0
-    compare: int = 0
+    compare: int = input[0]
     while i < len(input):
         if input[i] > compare:
             compare = input[i]
