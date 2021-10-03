495ecbbd20266e1ccfd44b1bc3d5a1a0692ce56e
Aaviele <aaviele@live.unc.edu>
2021-10-03 19:43:49 -0400

    Progress on EX 05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,44 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730322189"
 
-# Define your functions below
+def only_evens(xs: list[int]) -> list[int]:
+    """Return only even numbers."""
+    i: int = 0
+    evens: list[int] = []
+    while i < len(xs):
+        if xs[i] % 2 == 0:
+            evens.append(xs[i])
+        i += 1
+    return evens
+
+
+def sub(xs: list[int], start: int, end: int) -> list[int]:
+    """Generate list between start and end indices."""
+    subset: list[int] = []
+    if len(xs) <= 0 or start > len(xs) or end <= 0:
+        return subset
+    if start < 0:
+        start = 0
+    i: int = start
+    if end > len(xs) - 1:
+        end = len(xs) - 1
+        while i <= end:
+            subset.append(xs[i])
+            i += 1
+        return subset
+    else:
+        while i < end:
+            subset.append(xs[i])
+            i += 1
+        return subset
+
+
+def concat(list_1: list[int], list_2: list[int]) -> list[int]:
+    """Return list containing items from both lists."""
+    concat: list[int] = list_1
+    i: int = 0
+    while i < len(list_2):
+        concat.append(list_2[i])
+        i += 1
+    return concat
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,58 @@
 """Unit tests for list utility functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730322189"
+
+def test_only_evens_empty() -> None:
+    xs: list[int] = []
+    assert only_evens(xs) == []
+
+
+def test_only_evens_all_odds() -> None:
+    xs: list[int] = [1, 3, 5]
+    assert only_evens(xs) == []
+
+
+def test_only_evens_all_evens() -> None:
+    xs: list[int] = [2, 4, 6]
+    assert only_evens(xs) == [2, 4, 6]
+
+
+def test_sub_empty() -> None:
+    xs: list[int] = []
+    start: int = 1
+    end: int = 3
+    assert sub(xs, 1, 3) == []
+
+
+def test_sub_use_case_1() -> None:
+    xs: list[int] = [10, 20, 30]
+    start: int = 1
+    end: int = 4
+    assert sub(xs, 1, 4) == [20, 30]
+
+
+def test_sub_use_case_2() -> None:
+    xs: list[int] = [17, 39, 4, 763, 9]
+    start: int = 2
+    end: int = 4
+    assert sub(xs, 2, 4) == [4, 763]
+
+
+def test_concat_empty() -> None:
+    list_1: list[int] = []
+    list_2: list[int] = []
+    assert concat(list_1, list_2) == []
+
+
+def test_concat_use_case_1() -> None:
+    list_1: list[int] = [1, 2, 4]
+    list_2: list[int] = [5, 2, 9]
+    assert concat(list_1, list_2) == [1, 2,4, 5, 2, 9]
+
+
+def test_concat_use_case_2() -> None:
+    list_1: list[int] = [2, 4, 2]
+    list_2: list[int] = [-4, -2, -90]
+    assert concat(list_1, list_2) == [2, 4, 2, -4, -2, -90]
