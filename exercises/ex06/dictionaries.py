5c5f25b80a6be45c29db75497bc80df467dc818e
Aaviele <aaviele@live.unc.edu>
2021-10-17 20:06:16 -0400

    Progress on EX06

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,60 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730322189"
 
-# Define your functions below
+
+def main() -> None:
+     print(favorite_color({"Marc": "yellow", "Ezri": "blue", "Kris": "blue"}))
+
+
+def invert(given: dict[str, str]) -> dict[str, str]:
+    """Invert the keys and values of a dictionary."""
+    invert: dict[str, str] = dict()
+    just_keys: list[str] = list()
+    for key in given:
+        if len(invert) == 0:
+            just_keys.append(given[key])
+            invert[given[key]] = key
+        else:
+            if given[key] in just_keys:
+                raise KeyError("more than one of the same key")
+            else:
+                just_keys.append(given[key])
+                invert[given[key]] = key
+    return invert
+
+
+def favorite_color(names_and_colors: dict[str, str]) -> str:
+    """Check which color appears most frequently."""
+    colors: list[str] = list()
+    counting: dict[str,int] = dict()
+    favorite: str
+    for key in names_and_colors:
+        colors.append(names_and_colors[key])
+    for color in colors:
+        if color in counting:
+            counting[color] += 1
+        else:
+            counting[color] = 1
+    for item in counting:
+        max: int = 0
+        if counting[item] > 0:
+            max = counting[item]
+            favorite = item
+    return favorite
+
+
+def count(freq: list[str]) -> dict[str, int]:
+    """Create a dictionary that counts frequency of values."""
+    store: dict[str,int] = dict()
+    for item in freq:
+        if item in store:
+            store[item] += 1
+        else:
+            store[item] = 1
+
+    return store
+
+
+if __name__ == "__main__":
+    main()
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,53 @@
 """Unit tests for dictionary functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+from exercises.ex06.dictionaries import invert, favorite_color, count
 
 __author__ = "123456789"
+
+
+def test_inverse_empty() -> None:
+    """Test for invert, empty string."""
+    given: dict[str, str] = {}
+    assert invert(given) == {}
+
+
+def test_invert_case_1() -> None:
+    """Test for invert, use case 1."""
+    given: dict[str, str] = {'apple': 'cat'}
+    assert invert(given) == {'cat': 'apple'}
+
+
+def test_inverse_case_2() -> None:
+    """Test for invert, use case 2."""
+    given: dict[str, str] = {'mustard': 'ketchup'}
+    assert invert(given) == {'ketchup': 'mustard'}
+
+
+def test_favorite_color_tie() -> None:
+    """Test for favorite_color for tie for favorite."""
+    names_and_colors: dict[str, str] = {"Marc": "yellow", "Ezri": "blue", "Kris": "blue"}
+    assert favorite_color(names_and_colors) == 'blue'
+
+
+def test_favorite_colors_case_1() -> None:
+    """Test for favorite_color, use case 1."""
+    names_and_colors: dict[str, str] = {"Aaviele": "blue", "Kaitlyn": "green", "Sarah": "green"}
+    assert favorite_color(names_and_colors) == "green"
+
+
+def test_favorite_colors_case_2() -> None:
+    """Test for favorite_color, use case 2."""
+    names_and_colors: dict[str, str] = {"Heather": "yellow", "Jon": "green", "Bob": "yellow"}
+    assert favorite_color(names_and_colors) == "yellow"
+
+
+def test_count_empty() -> None:
+    """Test for count if empty."""
+    freq: list[str] = []
+    assert count(freq) == {}
+
+
+def test_count_case_1() -> None:
+    """Test for count, use case 1."""
+    freq: list[str] = ['blue', 'yellow', 'yellow']
+    assert count(freq) == {'blue': 1, 'yellow': 2}
